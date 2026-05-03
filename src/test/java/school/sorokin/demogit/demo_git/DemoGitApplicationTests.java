20f5b87dc1c2e9184dc461ce1e8c5608ddf69d78
sergey_cheredin <lotgames222@gmail.com>
2026-05-03 15:44:50 +0300

    init commit

---
+++ src/test/java/school/sorokin/demogit/demo_git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sorokin.demogit.demo_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
