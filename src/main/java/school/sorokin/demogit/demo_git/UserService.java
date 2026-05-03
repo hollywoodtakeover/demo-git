7670f2b34c2df7fa91073c5eab350dc1567f755d
sergey_cheredin <lotgames222@gmail.com>
2026-05-03 17:10:06 +0300

    Revert "test commit5"
    This reverts commit 87b31c5bcd62268745c28a7f613a1407f78a27bb.

--- src/main/java/school/sorokin/demogit/demo_git/UserService.java
+++ src/main/java/school/sorokin/demogit/demo_git/UserService.java
@@ -3,6 +3,5 @@ package school.sorokin.demogit.demo_git;
 public class UserService {
     public void helloWorld() {
         System.out.println("Привет мир");
-        System.out.println("ку");
     }
 }
